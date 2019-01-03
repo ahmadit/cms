9ae18e9a6e4b901fc27a905904812aef8b7cd15e
Ahmad <a@ahmad.it>
2019-01-03 21:31:39 +0000

    update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1 style='color:blue'>Hello There!</h1>"]
+    return ["<h1 style='color:blue'>Hello There!</h1><h7>version 1</h7>"]
